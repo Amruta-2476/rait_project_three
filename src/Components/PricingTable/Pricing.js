acfc13c5867b6d6b74ac2e8af9c97563ddd236e1
Amruta Mahesh Pawar <amr.paw.rt22@dypatil.edu>
2023-12-23 16:41:25 +0530

    done for the day

--- src/Components/PricingTable/Pricing.js
+++ src/Components/PricingTable/Pricing.js
@@ -29,14 +29,61 @@ const Pricing = () => {
                     status:false,
                 },
             }
+        },
+        service2:{
+            icon: 'icofont-tooth',
+            title: "Teeth Whitening",
+            amount: 299,
+            features: {
+                feature1: {
+                    name:"Lorem ipsum dolor sit",
+                    status:true,
+                },
+                feature2: {
+                    name:"Cubitur sollicitudin fentum",
+                    status:true,
+                },
+                feature3: {
+                    name:" Nullam interdum enim",
+                    status:true,
+                },
+                feature4: {
+                    name:" Donec ultricies metus",
+                    status:false,
+                },
+                feature5: {
+                    name:" Pellentesque eget nibh",
+                    status:false,
+                },
+            }
+        },
+        service3:{
+            icon: 'icofont-heart-beat',
+            title: "Heart Suggery",
+            amount: 399,
+            features: {
+                feature1: {
+                    name:"Lorem ipsum dolor sit",
+                    status:true,
+                },
+                feature2: {
+                    name:"Cubitur sollicitudin fentum",
+                    status:true,
+                },
+                feature3: {
+                    name:" Nullam interdum enim",
+                    status:true,
+                },
+                feature4: {
+                    name:" Donec ultricies metus",
+                    status:true,
+                },
+                feature5: {
+                    name:" Pellentesque eget nibh",
+                    status:true,
+                },
+            }
         }
-         // ,
-        // service2:{
-
-        // },
-        // service3:{
-
-        // }
     }
   return (
     <>
@@ -59,98 +106,10 @@ const Pricing = () => {
              <PriceService services={services.service1}/>
               {/* End Single Table*/}
               {/* Single Table */}
-              <div className="col-lg-4 col-md-12 col-12">
-                <div className="single-table">
-                  {/* Table Head */}
-                  <div className="table-head">
-                    <div className="icon">
-                      <i className="icofont icofont-tooth" />
-                    </div>
-                    <h4 className="title">Teeth Whitening</h4>
-                    <div className="price">
-                      <p className="amount">
-                        $299<span>/ Per Visit</span>
-                      </p>
-                    </div>
-                  </div>
-                  {/* Table List */}
-                  <ul className="table-list">
-                    <li>
-                      <i className="icofont icofont-ui-check" />
-                      Lorem ipsum dolor sit
-                    </li>
-                    <li>
-                      <i className="icofont icofont-ui-check" />
-                      Cubitur sollicitudin fentum
-                    </li>
-                    <li>
-                      <i className="icofont icofont-ui-check" />
-                      Nullam interdum enim
-                    </li>
-                    <li className="cross">
-                      <i className="icofont icofont-ui-close" />
-                      Donec ultricies metus
-                    </li>
-                    <li className="cross">
-                      <i className="icofont icofont-ui-close" />
-                      Pellentesque eget nibh
-                    </li>
-                  </ul>
-                  <div className="table-bottom">
-                    <a className="btn" href="#">
-                      Book Now
-                    </a>
-                  </div>
-                  {/* Table Bottom */}
-                </div>
-              </div>
+              <PriceService services={services.service2}/>
               {/* End Single Table*/}
               {/* Single Table */}
-              <div className="col-lg-4 col-md-12 col-12">
-                <div className="single-table">
-                  {/* Table Head */}
-                  <div className="table-head">
-                    <div className="icon">
-                      <i className="icofont-heart-beat" />
-                    </div>
-                    <h4 className="title">Heart Suggery</h4>
-                    <div className="price">
-                      <p className="amount">
-                        $399<span>/ Per Visit</span>
-                      </p>
-                    </div>
-                  </div>
-                  {/* Table List */}
-                  <ul className="table-list">
-                    <li>
-                      <i className="icofont icofont-ui-check" />
-                      Lorem ipsum dolor sit
-                    </li>
-                    <li>
-                      <i className="icofont icofont-ui-check" />
-                      Cubitur sollicitudin fentum
-                    </li>
-                    <li>
-                      <i className="icofont icofont-ui-check" />
-                      Nullam interdum enim
-                    </li>
-                    <li>
-                      <i className="icofont icofont-ui-check" />
-                      Donec ultricies metus
-                    </li>
-                    <li>
-                      <i className="icofont icofont-ui-check" />
-                      Pellentesque eget nibh
-                    </li>
-                  </ul>
-                  <div className="table-bottom">
-                    <a className="btn" href="#">
-                      Book Now
-                    </a>
-                  </div>
-                  {/* Table Bottom */}
-                </div>
-              </div>
+              <PriceService services={services.service3}/>
               {/* End Single Table*/}
             </div>
           </div>
